f5f55c0ddc589b04eddccfeee76eec4b380f39a2
HuangWei <huangwei.a180285@gmail.com>
2023-08-13 15:59:50 +0800

    draft local-test.sh

---
+++ local-test.sh
@@ -0,0 +1,5 @@
+# docker run --rm --volume="%CD%:/srv/jekyll" -it jekyll/jekyll sh -c "chown -R jekyll /usr/gem/ && jekyll new %site_name%" && cd %site_name%
+
+set -ex
+
+docker run --rm --volume="$(pwd):/srv/jekyll" -it jekyll/jekyll:4.2.2 sh -c "bundle install && bundle exec jekyll serve"
